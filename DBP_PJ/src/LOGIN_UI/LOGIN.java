a7cf940597ff2e2551381bada2a54d116d152b3a
PinkTultle <inseuk1007@naver.com>
2023-11-30 19:50:01 +0900

    first commit

---
+++ DBP_PJ/src/LOGIN_UI/LOGIN.java
@@ -0,0 +1,12 @@
+package LOGIN_UI;
+
+import javax.swing.JFrame;
+
+public class LOGIN extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DBP_PJ/src/MAIN_UI/MAIN_.java
@@ -0,0 +1,12 @@
+package MAIN_UI;
+
+import javax.swing.JFrame;
+
+public class MAIN_ extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
