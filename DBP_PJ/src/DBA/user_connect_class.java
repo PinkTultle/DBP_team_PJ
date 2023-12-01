a482cff5e53c8c39effe78c5741eee02b70af9cc
PinkTultle <inseuk1007@naver.com>
2023-12-01 18:57:43 +0900

    db연결 파일 생성

---
+++ DBP_PJ/src/DBA/user_connect_class.java
@@ -0,0 +1,39 @@
+package DBA;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class user_connect_class {
+	
+	Connection con = null;
+	ResultSet rs;
+	Statement stmt;
+	PreparedStatement pstmt;
+	CallableStatement callstmt;
+	
+	String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	String id = "ABD";
+	String pw = "1234";
+	
+	public user_connect_class() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.print("드라이브 적재 성공");
+			con = DriverManager.getConnection(url, id, pw);
+			System.out.println("데이터 베이스 연결 성공");
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("드라이버를 찾을 수 없습니다.");
+		} catch (SQLException e) {
+			System.out.println("연결에 실패하였습니다.");
+		}
+	}
+	
+
+}
