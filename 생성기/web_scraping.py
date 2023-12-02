e70db19315bd2a92964c7659e7ae3b8c80fec05a
PinkTultle <inseuk1007@naver.com>
2023-12-02 22:48:56 +0900

    수정중

--- 생성기/web_scraping.py
+++ 생성기/web_scraping.py
@@ -3,10 +3,10 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-url ="https://namu.wiki/w/%EC%A4%91%EA%B5%AD%20%EC%9A%94%EB%A6%AC"
+url ="https://namu.wiki/w/%EB%AF%B8%EA%B5%AD%20%EC%9A%94%EB%A6%AC"
 
 # 엑셀 파일로 저장하기
-filename = "중국요리목록.csv"
+filename = "미국요리목록.csv"
 f = open(filename, "w", encoding="utf-8-sig", newline="")
 writer = csv.writer(f)
 
